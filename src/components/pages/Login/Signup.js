a61e1a2d2eb1ec54d6b7a57ed9ba666a8fe12e83
dongmyoungLee <pajang1515@daum.net>
2023-05-04 17:23:49 +0900

    회원가입폼 추가

--- src/components/pages/Login/Signup.js
+++ src/components/pages/Login/Signup.js
@@ -1,21 +1,15 @@
 import classes from '../../../styles/pages/login/login.module.css';
 import classes2 from '../../../styles/pages/login/signup.module.css';
-import RadioGroup from "../../atoms/RadioGroup";
-import Radio from "../../atoms/Radio";
 import {useState} from "react";
 import Input from "../../atoms/Input";
 import Button from "../../atoms/Button";
 import {Mobile, PC} from "../../config/Responsive";
 
 const Login = () => {
-  const [isLoginType, setIsLoginType] = useState('general');
   const [idInput, setIdInput] = useState('');
   const [passInput, setPassInput] = useState('');
   const [error, setError] = useState(false);
 
-  const radioChangeHandler = (e) => {
-    setIsLoginType(e.target.value);
-  }
   const generalIdInputHandler = (e) => {
     setIdInput(e.target.value);
   }
@@ -25,59 +19,51 @@ const Login = () => {
   const generalOnSubmitHandler = (e) => {
     e.preventDefault();
 
-    // 일반회원 로그인 후 에러처리하자
-
   }
 
-  const companyIdInputHandler = (e) => {
-    setIdInput(e.target.value);
-  }
-  const companyPassInputHandler = (e) => {
-    setPassInput(e.target.value);
-  }
-  const companyOnSubmitHandler = (e) => {
-    e.preventDefault();
 
-    // 기업회원 로그인 후 에러처리 하자
-  }
 
-  const labelName = isLoginType === 'general' ? '아이디(이메일)' : '기업 아이디(이메일)';
-  const submitEvt = isLoginType === 'general' ? generalOnSubmitHandler : companyOnSubmitHandler;
-  const idChangeEvt = isLoginType === 'general' ? generalIdInputHandler : companyIdInputHandler;
-  const passChangeEvt = isLoginType === 'general' ? generalPassInputHandler : companyPassInputHandler;
   const errorParam = '이메일과 비밀번호가 일치하지 않습니다.';
 
-
-  const pcLoginForm = <form className={classes2.signupGeneralForm} onSubmit={submitEvt}>
-                        <Input label={labelName} onChange={idChangeEvt} input={{
+  const pcLoginForm = <form className={classes2.signupGeneralForm} onSubmit={generalOnSubmitHandler}>
+                        <Input label='아이디(이메일)' onChange={generalIdInputHandler} input={{
                           type : 'text',
                           placeholder : 'example@email.com'
                         }} />
-                        <Input label='비밀번호' onChange={passChangeEvt} input={{
+                        <Input label='비밀번호' onChange={generalPassInputHandler} input={{
                           type : 'password',
                           placeholder : '********',
                         }} />
-                        <Input label={labelName} onChange={idChangeEvt} input={{
-                          type : 'text',
-                          placeholder : 'example@email.com'
+                        <Input label='비밀번호 확인' input={{
+                          type : 'password',
+                          placeholder : '********',
                         }} />
-                        <Input label={labelName} onChange={idChangeEvt} input={{
+                        <Input label='생년월일' input={{
                           type : 'text',
-                          placeholder : 'example@email.com'
+                          placeholder : '1999-01-01',
+                        }} />
+                        <Input label='연락처' input={{
+                          type : 'text',
+                          placeholder : '010-1234-1234'
+                        }} />
+                        <Input label='주소' input={{
+                          type : 'text',
+                          placeholder : '서울특별시 논현로'
                         }} />
                         {error && <p className={classes.error}>{errorParam}</p>}
                         <Button btn={{
                           type : '',
                           value : '회원가입'
                         }} />
+
                       </form>;
 
-  const mobileLoginForm = <form className={classes2.mobileSignupGeneralForm} onSubmit={submitEvt}>
-                            <Input label={labelName} onChange={idChangeEvt} input={{
+  const mobileLoginForm = <form className={classes2.mobileSignupGeneralForm} onSubmit={generalOnSubmitHandler}>
+                            <Input label='아이디(이메일)' onChange={generalIdInputHandler} input={{
                               type : 'text',
                               placeholder : 'example@email.com'
                             }} />
-                            <Input label='비밀번호' onChange={passChangeEvt} input={{
+                            <Input label='비밀번호' onChange={generalPassInputHandler} input={{
                               type : 'password',
                               placeholder : '********',
                             }} />
@@ -94,14 +80,6 @@ const Login = () => {
             <div className={classes.wrap}>
               <article className={classes.article}>
                 <div className={classes.formWrap}>
-                  <RadioGroup>
-                    <Radio name="contact" value="general" defaultChecked onChange={radioChangeHandler}>
-                      <p>일반회원</p>
-                    </Radio>
-                    <Radio name="contact" value="company" onChange={radioChangeHandler}>
-                      기업회원
-                    </Radio>
-                  </RadioGroup>
                   <div>
                     <div className={classes.formOption}>
                       {pcLoginForm}
@@ -116,14 +94,6 @@ const Login = () => {
           <section className={classes.mobileSection}>
             <article className={classes.mobileArticle}>
               <div className={classes.mobileFormWrap}>
-                <RadioGroup>
-                  <Radio name="contact" value="general" defaultChecked onChange={radioChangeHandler}>
-                    <p>일반회원</p>
-                  </Radio>
-                  <Radio name="contact" value="company" onChange={radioChangeHandler}>
-                    기업회원
-                  </Radio>
-                </RadioGroup>
                 <div>
                   <div className={classes.formOption}>
                     {mobileLoginForm}
