6fd36571826f181218cbc999ac77fc5e5bb4c858
dongmyoungLee <pajang1515@daum.net>
2023-05-08 13:26:44 +0900

    CD test

--- src/common/Menus.js
+++ src/common/Menus.js
@@ -7,7 +7,7 @@ import myAccount from '../asset/images/myaccount.png';
 
 const headerMenuList = [
     {
-      menuName : '인채 채용',
+      menuName : '인채 채용1',
       menuLink : '/humanResources'
     },
     {
