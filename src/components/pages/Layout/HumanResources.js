e2db2c4387e2f797ef4526ce28b0b03a5d7db9c8
dongmyoungLee <pajang1515@daum.net>
2023-05-28 11:43:53 +0900

    API test

--- src/components/pages/Layout/HumanResources.js
+++ src/components/pages/Layout/HumanResources.js
@@ -1,5 +1,6 @@
 import Layout from "../../blocks/Layout";
 import {testGetApi} from "../../../common/api/ApiGetService";
+import {findUserJobInfo} from "../../../common/api/ApiPostService";
 
 
 const HumanResources = () => {
@@ -14,12 +15,22 @@ const HumanResources = () => {
     })
   }
 
+  const testApiHandler2 = async () => {
+
+    findUserJobInfo('pajang1515@daum.net').then((res) => {
+        console.log(res);
+    }).catch((error) => {
+        console.log(error);
+    })
+  }
+
 
   return (
       <>
         <Layout >
           <p>인재 채용 페이지</p>
           <button onClick={testApiHandler} >testAPI</button>
+          <button onClick={testApiHandler2} >testJOBAPI</button>
         </Layout>
       </>
   );
